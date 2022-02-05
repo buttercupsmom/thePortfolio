056f77ba2fcfa2cc023e5465bc788e5098a60ec2
Rachel <yarkony.rachel@gmail.com>
2022-02-05 13:45:10 -0600

    1st draft

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const contactBtn = document.getElementById("contactBtn");
+const aboutMeClick = document.getElementById("aboutMeClick");
+const workClick = document.getElementById("workClick");
+
+
+// functions
