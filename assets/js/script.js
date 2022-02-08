0cab953d0a510f84bb4da1a63fa318155658999a
Rachel <yarkony.rachel@gmail.com>
2022-02-08 14:05:17 -0600

    nav links clickable

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const contactBtn = document.getElementById("contactBtn");
-const aboutMeClick = document.getElementById("aboutMeClick");
-const workClick = document.getElementById("workClick");
-
-
-// functions
